367e3bed77ed6d592a13dc0a4680319ab2128ce5
@nyalla <naveenyalla22@gmail.com>
2021-10-17 20:05:43 +0530

    Update ExampleController.java

--- src/main/java/com/example/demo/controller/ExampleController.java
+++ src/main/java/com/example/demo/controller/ExampleController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.demo.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
